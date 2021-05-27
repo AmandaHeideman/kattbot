0615a6f037d9128cd475aaa956c0c12e49d9083b
amandaheideman <hedieman.amanda@gmail.com>
2021-05-27 10:19:02 +0200

    create kattbot

---
+++ index.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+const fetch = require('node-fetch');
+
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', async message => {
+	if (message.content === prefix) {
+		const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+		message.channel.send(file);
+	}
+});
+
+client.login(token);
